adfa03e0aff531a1937a2b4f097f9f31a4685db2
joe <jchimien@gmail.com>
2019-10-27 23:21:46 -0500

    test JsObject in call, add methods

--- build.sbt
+++ build.sbt
@@ -1,9 +1,14 @@
 name := "lightningcli"
 
-version := "0.1"
+version := "0.1.0"
+
+description := "Lightning Client"
+
+organization := "mathbot.com"
 
 scalaVersion := "2.13.1"
 
-libraryDependencies += "org.scala-sbt.ipcsocket" % "ipcsocket" % "1.0.0"
-
-libraryDependencies += "io.spray" % "spray-json_2.13" % "1.3.5"
+libraryDependencies ++= Seq(
+  "org.scala-sbt.ipcsocket" % "ipcsocket" % "1.0.0",
+  "io.spray" % "spray-json_2.13" % "1.3.5"
+)
--- src/main/scala/com/mathbot/lightning/LightningClient.scala
+++ src/main/scala/com/mathbot/lightning/LightningClient.scala
@@ -4,57 +4,13 @@ import java.io.{BufferedReader, InputStreamReader, PrintWriter}
 import java.util.concurrent.atomic.AtomicInteger
 
 import org.scalasbt.ipcsocket.UnixDomainSocket
-import spray.json.{DefaultJsonProtocol, _}
+import spray.json._
 
-case class Request(method: String, id: Int, params: Array[String] = Array.empty[String])
+import scala.collection.immutable.List
 
-trait MyJsonProtocol extends DefaultJsonProtocol {
-  implicit val getinfoFormat: RootJsonFormat[Request] = jsonFormat3(Request)
-}
-
-trait Response[T] {
-
-  def id: Int
-  def jsonrpc: Double = 2.0
-  def response: T
-}
-
-case class ResultBis(
-                      id: Double,
-                      payment_hash: String,
-                      destination: String,
-                      msatoshi: Double,
-                      amount_msat: String,
-                      msatoshi_sent: Double,
-                      amount_sent_msat: String,
-                      created_at: Double,
-                      status: String,
-                      payment_preimage: String,
-                      bolt11: String
-                    )
-
-case class Payments(
-                     id: Double,
-                     payment_hash: String,
-                     destination: String,
-                     msatoshi: Double,
-                     amount_msat: String,
-                     msatoshi_sent: Double,
-                     amount_sent_msat: String,
-                     created_at: Double,
-                     status: String,
-                     payment_preimage: String,
-                     bolt11: String
-                   )
-
-case class ListPayments(id: Int, response: Payments) extends Response[Payments]
-
-case class PayResponse(id: Int, response: ResultBis) extends Response[ResultBis]
 
 
 class LightningClient(path: String = "/var/lib/docker/volumes/generated_clightning_bitcoin_datadir/_data/lightning-rpc") extends MyJsonProtocol {
-
-
 
   private val atom = new AtomicInteger()
 
@@ -62,7 +18,7 @@ class LightningClient(path: String = "/var/lib/docker/volumes/generated_clightni
   private val in = new BufferedReader(new InputStreamReader(client.getInputStream))
   private val out = new PrintWriter(client.getOutputStream, true)
 
-  def call(method : String, params: String*) = {
+  def call(method : String, params: String*): String = {
 
     val id = atom.getAndIncrement()
     val info = Request(method = method, id = id, params = params.toArray)
@@ -74,21 +30,78 @@ class LightningClient(path: String = "/var/lib/docker/volumes/generated_clightni
 
   }
 
-  def close = {
-    client.close()
+  def call(method: String, params: JsObject) = {
+
+    val id = atom.getAndIncrement()
+    val info = JsObject(
+      "method" -> JsString(method),
+      "id" -> JsNumber(id),
+      "params" -> params
+    )
+    println(info.toJson.prettyPrint)
+    out.println(info.toJson)
+    val line = in.readLine
+    println(line)
+    line
   }
 
+  /**
+    * Sets up automatic cleaning of expired invoices. {cycle_seconds} sets
+    * the cleaning frequency in seconds (defaults to 3600) and {expired_by}
+    * sets the minimum time an invoice should have been expired for to be
+    * cleaned in seconds (defaults to 86400).
+    * @param cycleSeconds
+    * @param expiredBy
+    */
+  def autocleaninvoice(cycleSeconds: Option[Int], expiredBy: Option[Int]) = {
+    call("autocleaninvoice", JsObject(
+      "cycle_seconds" -> cycleSeconds.map(JsNumber(_)).getOrElse(JsNull),
+      "expired_by" -> expiredBy.map(JsNumber(_)).getOrElse(JsNull)
+    ))
+  }
+
+  def pay(bolt11: String): String =
+    call("pay", bolt11)
 
 
+  def listPayments(bol11: String) : String  = listPayments(Some(bol11))
 
-  // fixme: not parsing
-//  call("listpeers")
-//  call("listinvoices")
+  def listPayments(bolt11: Option[String]): String =
+    call("listpayments", bolt11 getOrElse Array.empty)
 
-//  call("getinfo")
-//  call("help")
-  //call("pay", Seq("lntb100n1pwmv0wdpp5jse9v4pvwywwt7xucvzmnrzr8dlwm7t0tx9c38juncvus02yapxqdqqcqzpgxqyz5vqcn36xtue6yzcmf9nr70h6n0uhfsg6jwc320w66r9hh6zsymqyeh9jwkdqge5u2slpqjdw8j8kggrn3re68pw7ysylxvrt3f9tdf9c0gp4vrfxr"))
+  def listTransactions = call("listtransactions")
 
+  /**
+    * Show invoice {label} (or all, if no {label))
+    * @param invoice
+    * @return
+    */
+  def listInvoices(invoice: Option[String]) = call("listinvoices", invoice getOrElse Array.empty)
+
+
+  /**
+    * Show all nodes in our local network view, filter on node {id}
+    * if provided
+    *
+    * @param node
+    * @return
+    */
+  def listNodes(node: Option[String]) = call("listnodes", node getOrElse Array.empty)
+
+  def help: String = call("help")
+
+  def getinfo: String = call("getinfo")
+
+  def decodePay(bolt11: String) = call("decodepay", bolt11)
+
+  def payStatus(bolt11: Option[String]) = call("paystatus", bolt11 getOrElse Array.empty)
+
+  def setChannelFee(id: Option[String], base: Option[Int], ppm: Option[Int]) = {
+    call("setchannelfee")
+  }
+
+  def close(): Unit =
+    client.close()
 
 
 }
@@ -98,10 +111,15 @@ object LightningClientApp extends App {
 
   val client = new LightningClient()
 
-//  client.call("pay", "lntb100n1pwmvnwspp5qe5jtqym2nfexgvzawcsjhejyjqkgcvq305nywz8m5edsle7gghqdqqcqzpgxqyz5vqz7taykkfas4gszlqj6sh3tmht03le4q3uc988xdfnpkyejkrnrwztlm8lluccfn7kr4330dylxkzn58d2s5f07zq9cpnhwvq07nla2cq6de2z0")
-  client.call("listpayments", "lntb100n1pwmv0wdpp5jse9v4pvwywwt7xucvzmnrzr8dlwm7t0tx9c38juncvus02yapxqdqqcqzpgxqyz5vqcn36xtue6yzcmf9nr70h6n0uhfsg6jwc320w66r9hh6zsymqyeh9jwkdqge5u2slpqjdw8j8kggrn3re68pw7ysylxvrt3f9tdf9c0gp4vrfxr")
-//  client.call("listpayments",  "lntb100n1pwmvnwspp5qe5jtqym2nfexgvzawcsjhejyjqkgcvq305nywz8m5edsle7gghqdqqcqzpgxqyz5vqz7taykkfas4gszlqj6sh3tmht03le4q3uc988xdfnpkyejkrnrwztlm8lluccfn7kr4330dylxkzn58d2s5f07zq9cpnhwvq07nla2cq6de2z0")
+  client.autocleaninvoice(None, None)
 
-  client.close
-  sys.exit(0)
+  client.listPayments("lntb100n1pwmv0wdpp5jse9v4pvwywwt7xucvzmnrzr8dlwm7t0tx9c38juncvus02yapxqdqqcqzpgxqyz5vqcn36xtue6yzcmf9nr70h6n0uhfsg6jwc320w66r9hh6zsymqyeh9jwkdqge5u2slpqjdw8j8kggrn3re68pw7ysylxvrt3f9tdf9c0gp4vrfxr")
+  client.close()
+//  client.call("pay", "lntb100n1pwmvnwspp5qe5jtqym2nfexgvzawcsjhejyjqkgcvq305nywz8m5edsle7gghqdqqcqzpgxqyz5vqz7taykkfas4gszlqj6sh3tmht03le4q3uc988xdfnpkyejkrnrwztlm8lluccfn7kr4330dylxkzn58d2s5f07zq9cpnhwvq07nla2cq6de2z0")
+//  client.call("listpayments",  "lntb100n1pwmvnwspp5qe5jtqym2nfexgvzawcsjhejyjqkgcvq305nywz8m5edsle7gghqdqqcqzpgxqyz5vqz7taykkfas4gszlqj6sh3tmht03le4q3uc988xdfnpkyejkrnrwztlm8lluccfn7kr4330dylxkzn58d2s5f07zq9cpnhwvq07nla2cq6de2z0")
+  //  call("listpeers")
+  //  call("listinvoices")
+
+  //  call("getinfo")
+  //  call("help")
 }
